a60c4b2a1ba1357fa18243a97026757db47ceb4d
bigstring-dev <bigstring.dev@gmail.com>
2026-04-12 04:50:38 +0900

    test: 인프라 재구성으로 인한 테스트
    - application-prod.yml
      - DB 생성을 위한 위한 ddl-auto 변경
      - quartz 테이블 생성을 위한 initialize-schema 변경
    
    - InitializerConfig.java
      - 데이터 초기화후 초기 데이터 생성을 위한 로직 수정

--- src/main/java/sumcoda/boardbuddy/config/InitializerConfig.java
+++ src/main/java/sumcoda/boardbuddy/config/InitializerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import sumcoda.boardbuddy.service.BadgeImageService;
 import sumcoda.boardbuddy.service.MemberService;
 
+import java.time.YearMonth;
 import java.util.TimeZone;
 
 
@@ -19,30 +20,15 @@ public class InitializerConfig {
 
     private final BadgeImageService badgeImageService;
 
-//    // 성능 개선용 코드
-//    private final RedisTemplate<String, String> chatMessageRedisZSetTemplate;
-//    private final RedisTemplate<String, byte[]> chatMessageRedisTemplate;
-
-//    private final TestDataSetInitializeService testDataSetInitializeService;
-
     @Bean
     public ApplicationRunner initializer() {
         return args -> {
 
-//            // 성능 개선용 코드
-//            Set<String> keys = chatMessageRedisTemplate.keys("chat:*");
-//            if (!keys.isEmpty()) {
-//                chatMessageRedisZSetTemplate.delete(keys);
-//                chatMessageRedisTemplate.delete(keys);
-//            }
-//
-//            testDataSetInitializeService.initializeTestDataSet();
-
             // 프로덕션 코드
             // ddl-auto none 으로 수정후 아래 로직 비활성화
-//            memberService.createAdminAccount();
-//            memberService.createInitTestAccounts();
-//            badgeImageService.assignBadgesToInitTestMembers(YearMonth.now().minusMonths(1));
+            memberService.createAdminAccount();
+            memberService.createInitTestAccounts();
+            badgeImageService.assignBadgesToInitTestMembers(YearMonth.now().minusMonths(1));
         };
     }
 
