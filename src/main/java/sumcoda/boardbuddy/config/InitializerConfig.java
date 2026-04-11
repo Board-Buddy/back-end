01353145926e7df236415cdd3e2c0fe68269b236
bigstring-dev <bigstring.dev@gmail.com>
2026-04-12 05:32:59 +0900

    refactor: 인프라 재구성으로 인한 추가 리팩토링
    - application-prod.yml
      - DB 유지를 위한 위한 ddl-auto 변경
      - quartz 테이블 유지를 위한 initialize-schema 변경
    
    - InitializerConfig.java
      - 데이터 초기화후 초기 데이터 유지를 위한 로직 삭제

--- src/main/java/sumcoda/boardbuddy/config/InitializerConfig.java
+++ src/main/java/sumcoda/boardbuddy/config/InitializerConfig.java
@@ -2,35 +2,14 @@ package sumcoda.boardbuddy.config;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import sumcoda.boardbuddy.service.BadgeImageService;
-import sumcoda.boardbuddy.service.MemberService;
 
-import java.time.YearMonth;
 import java.util.TimeZone;
 
 
 @Configuration
 @RequiredArgsConstructor
 public class InitializerConfig {
-
-    private final MemberService memberService;
-
-    private final BadgeImageService badgeImageService;
-
-    @Bean
-    public ApplicationRunner initializer() {
-        return args -> {
-
-            // 프로덕션 코드
-            // ddl-auto none 으로 수정후 아래 로직 비활성화
-            memberService.createAdminAccount();
-            memberService.createInitTestAccounts();
-            badgeImageService.assignBadgesToInitTestMembers(YearMonth.now().minusMonths(1));
-        };
-    }
 
     @PostConstruct
     public void setTimeZone(){
